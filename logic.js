6f1cebf52f4d9a6cde33c7ae244174a1a00612fa
BecoolGhub <leninkumar.official@gmail.com>
2026-03-31 16:49:21 +0530

    feat : added new lines in all of the file

--- logic.js
+++ logic.js
@@ -19,13 +19,92 @@ console.log(age); */
 
 //number data type
 
-let nAME =  "lenin kumar s"
+/* let nAME =  "lenin kumar s"
 console.log(typeof nAME);
 console.log(nAME);
 
  nAME = '05.05.2000';
 console.log(typeof nAME);
-console.log(nAME);
+console.log(nAME); */
+
+//number data type...
+
+// let age = 65265.526;
+
+// console.log(typeof age);
+
+// let infi = Infinity;
+// let neginfi = -Infinity;
+
+// console.log(typeof infi);
+// console.log(typeof neginfi)
+
+//string data type...
+
+// let a = 348, b =  39834;
+
+// let embed = ` sum of 348 + 39834 is ${a+b}`;
+
+// console.log(embed);
+
+// boolean  operator
+
+// let name1 = true;
+// console.log(name1);
+
+//null operator
+
+// let name11 = null;
+// console.log(`4. ${name11} `);
+
+//type conversion 
+
+// console.log("type conversion...")
+
+// let t = 34;
+// let convert = t--;
+// console.log(convert);
+
+// let u = 34;
+// let convert1 = u++;
+// console.log(convert1);
+
+// let a = 12;
+// let b = 13;
+
+// let c = ++a + b--;
+
+// console.log(c);
+
+//type conversion
+
+// console.log(null < 0);
+// console.log(null > 0)
+// console.log(null <= 0)
+// console.log(null >= 0)
+// console.log(null == 0)
+
+//conditions 
+
+// let lenin = (54 > 93)? 'what you want' : 'fuck you guys...';
+
+// console.log(lenin);
+
+//switch case 
+
+switch (2){
+    case 1:
+        console.log("hello");
+        break;
+    case 2:
+        console.log("i am ");
+        break;
+    case 3:
+        console.log("lenin kumar");
+        break;
+    default:
+        console.log("nobody...");
+}
 
 
 
