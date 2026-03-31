a03b8fd049c963bfc26de6bf5009fa19d3a29731
BecoolGhub <leninkumar.official@gmail.com>
2026-03-31 09:44:15 +0530

    feat : added new line

--- logic.js
+++ logic.js
@@ -1,2 +1,33 @@
-console.log("hi guys this is lenin");
+//variables
 
+/* console.log("hi guys this is lenin");
+
+let name = "lenin kumar s";
+
+let age = "35", 
+current_status="studying";
+
+console.log(age, "\n",current_status); */
+
+//constant
+
+/* const nAME = '05.05.2000';
+
+const age = calAge(nAME);
+
+console.log(age); */
+
+//number data type
+
+let nAME =  "lenin kumar s"
+console.log(typeof nAME);
+console.log(nAME);
+
+ nAME = '05.05.2000';
+console.log(typeof nAME);
+console.log(nAME);
+
+
+
+
+  
